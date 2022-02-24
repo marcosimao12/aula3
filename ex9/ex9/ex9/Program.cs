aa3d52dfbf40dfdf10a98496a77fcaf2a8f9da94
marcosimao12 <marcosimaocarvalho@gmail.com>
2022-02-24 15:04:38 +0000

    ex9

---
+++ ex9/ex9/ex9/Program.cs
@@ -0,0 +1,15 @@
+﻿string nome = "marco simao";
+Console.WriteLine(nome.Length);
+
+Console.WriteLine(nome + " carvalho");
+
+Console.WriteLine(nome.ToUpper());
+
+Console.WriteLine(nome.Substring(6, 5));
+
+Console.WriteLine(nome.IndexOf("simao"));
+
+int x = "Braga".CompareTo("qwerty");
+Console.WriteLine(x);
+
+Console.WriteLine(nome.Trim());
