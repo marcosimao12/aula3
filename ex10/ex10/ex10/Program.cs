3517177c934ab1529711d983f56a40fd3509e3b4
marcosimao12 <marcosimaocarvalho@gmail.com>
2022-02-24 15:54:15 +0000

    ex10

---
+++ ex10/ex10/ex10/Program.cs
@@ -0,0 +1,12 @@
+﻿sbyte S = 34;
+byte B = 250;
+short SH = 32000;
+ushort U = 60000;
+int I = 1000000012;
+long UI = 3500000000;
+ulong L = 1532333333334581239;
+ulong F = 14532333333334581239;
+
+
+
+Console.WriteLine(S, B, SH, U, I, UI, L, F);
