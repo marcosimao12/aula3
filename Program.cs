7103b4a3163af1cee55e9e4aafd055ebb23bf848
marcosimao12 <marcosimaocarvalho@gmail.com>
2022-02-24 14:36:13 +0000

    ex8

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//ex8
+
+
+
+Console.WriteLine(4 == 5);
+Console.WriteLine(4 != 6);
+Console.WriteLine(4 > 5);
+Console.WriteLine(4 < 5 && 6 > 10);
+Console.WriteLine(40 < 50 || 60 > 90);
+Console.WriteLine(!(40 < 50 || 60 > 90));
+
